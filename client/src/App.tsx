b080b998570abead43e98bb6c3d0eea6fd45803c
hamzaTheZee <hamza.zeeshan7163@gmail.com>
2026-04-29 19:37:13 +0500

    Made the home page and connect all the pages to it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import Login from "./pages/Login/Login";
 import Register from "./pages/Login/Register";
 import CreateDonation from "./pages/Donation/CreateDonation";
-import MyDonations from "./pages/Donation/MyDonations";
-import DonationCharts from "./pages/Donation/DonationCharts";
+import HomePage from "./pages/Home/HomePage";
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (<> <Login />
-    < Register />
-    < CreateDonation />
-    <MyDonations />
-    <DonationCharts />
-  </>);
-}
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/create-donation" element={<CreateDonation />} />
+      <Route path="/signin" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+      <Route path="/contact" element={<Login />} />
+    </Routes>
+  );
+};
 
 export default App;
