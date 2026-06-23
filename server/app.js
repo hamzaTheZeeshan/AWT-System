3fd963addff491f3a86323c2cbd373edc97fd02e
Habib332 <ha8795691@gmail.com>
2026-06-23 15:29:28 +0500

    we up

--- server/app.js
+++ server/app.js
@@ -19,6 +19,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`CORS middleware triggered for ${req.method} ${req.url}`);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("AWT Backend API is running");
 });
